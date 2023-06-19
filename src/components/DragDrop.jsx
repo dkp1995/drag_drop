dbe8abdfa6dab0aaaf58a2352930981f6060410b
Dileep pale <neverbackdown.dilip@gmail.com>
2023-06-20 00:31:31 +0530

    one_more_image_added

--- src/components/DragDrop.jsx
+++ src/components/DragDrop.jsx
@@ -6,7 +6,9 @@ import { useDrop } from 'react-dnd'
 const ImageList = [ 
       { id: 1 , url : 'https://www.freecodecamp.org/news/content/images/2022/09/jonatan-pie-3l3RwQdHRHg-unsplash.jpg'},
       { id: 2 , url : 'https://img.freepik.com/free-photo/flowing-purple-mountain-spiral-bright-imagination-generated-by-ai_188544-9853.jpg'},
-      { id: 3 , url :  'https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60'}
+      { id: 3 , url :  'https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60'},
+      { id: 4 , url :  'https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60'}
+
 ]
 
 const DragDrop = () => {
